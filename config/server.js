2c3591989834acf957fcb996da556d943a6a9052
Alexander Noack <61937223+L3Roth@users.noreply.github.com>
2025-01-05 15:31:37 +0100

    Update server.js

--- config/server.js
+++ config/server.js
@@ -1,5 +1,5 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', 'pia.digital-tracht.de'),
+  //host: env('HOST', 'pia.digital-tracht.de'),
   port: env.int('PORT', 80),
   app: {
     keys: env.array('APP_KEYS'),
